3d1a67cfb85d2ca6ec137e4447ed4539d9fc340d
Tung <phamduytung2002Gmail.com>
2022-03-07 01:44:36 +0700

    add Session 1, 2, 3

---
+++ Session 1/Ridge Regression/RidgeRegression.py
@@ -0,0 +1,87 @@
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import KFold
+
+class RidgeRegression:
+    def __init__(self):
+        return
+    
+    def fit(self, X_train, Y_train, LAMBDA):
+        assert len(X_train.shape) == 2 and X_train.shape[0] == Y_train.shape[0]
+        W = np.linalg.inv(X_train.transpose()@X_train + LAMBDA * np.identity(X_train.shape[1])) \
+                     @(X_train.T)@Y_train
+        return W
+
+    def fitGrad(self, X_train, Y_train, LAMBDA, learningRate, maxEpochs=100, batchSize=128):
+        W = np.random.rand(X_train.shape[1])
+        lastLoss = 1e10
+        for epoch in range(maxEpochs):
+            arr = np.array(range(X_train.shape[0]))
+            np.random.shuffle(arr)
+            X_train, Y_train = X_train[arr], Y_train[arr]
+            numMinibatch = int(np.ceil(X_train.shape[0]/batchSize))
+            for i in range(numMinibatch):
+                index = i*batchSize
+                X_train_sub = X_train[index:index+batchSize]
+                Y_train_sub = Y_train[index:index+batchSize]
+                grad = X_train_sub.T@(X_train_sub@W-Y_train_sub) + LAMBDA*W
+                W = W - learningRate*grad
+            newLoss = self.computeRSS(self.predict(W, X_train), Y_train)
+            if np.abs(newLoss-lastLoss) < 1e-10:
+                break
+            lastLoss = newLoss
+        return W
+
+    def predict(self, W, X_test):
+        return X_test@W
+
+    def computeRSS(self, Y_predicted, Y_test):
+        return np.sum((Y_predicted-Y_test)**2)/Y_predicted.shape[0] 
+
+    def getTheBestLambda(self, X, Y):
+        def crossValid(numFolds, LAMBDA):
+            kf = KFold(n_splits=numFolds)
+            sumRSS = 0
+            for trainID, valID in kf.split(X):
+                W = self.fit(X[trainID], Y[trainID], LAMBDA = LAMBDA)
+                Y_pred = self.predict(W, X[valID])
+                sumRSS += self.computeRSS(Y_pred, Y[valID])
+            aveRSS = sumRSS/numFolds
+            return aveRSS
+
+        def rangeScan(bestLAMBDA, minRSS, LAMBDAVals):
+            for curLAMBDA in LAMBDAVals:
+                aveRSS = crossValid(numFolds=5, LAMBDA=curLAMBDA)
+                if aveRSS < minRSS:
+                    bestLAMBDA = curLAMBDA
+                    minRSS = aveRSS
+            return bestLAMBDA, minRSS
+
+        bestLAMBDA, minRSS = rangeScan(bestLAMBDA=1, minRSS=1e10, LAMBDAVals=range(50))
+        LAMBDAVals = [k/1000 for k in range(max(0,(bestLAMBDA-1)*1000), (bestLAMBDA+1)*1000, 1)]
+        bestLAMBDA, minRSS = rangeScan(bestLAMBDA=bestLAMBDA, minRSS=minRSS, LAMBDAVals=LAMBDAVals)
+        return bestLAMBDA
+
+def normalizeAndAddOnes(X):
+    X = np.array(X)
+    X_max = np.max(X, axis=0)
+    X_min = np.min(X, axis=0)
+    X_normalized = (X-X_min)/(X_max-X_min)
+    ones = np.ones(X_normalized.shape[0])
+    return np.column_stack((ones,X_normalized))
+
+if __name__=="__main__":
+    data = pd.read_csv("x28.csv", delimiter='\s+')
+    data = data.iloc[:, 1:].values
+    data = normalizeAndAddOnes(data)
+    X_train, X_test = data[:50, :-1], data[50:, :-1]
+    Y_train, Y_test = data[:50, -1], data[50:, -1]
+
+    LR = RidgeRegression()
+    LAMBDA = LR.getTheBestLambda(X_train, Y_train)
+    LAMBDA = LR.getTheBestLambda(X_train, Y_train)
+    # W = LR.fit(X_train, Y_train, LAMBDA)
+    W = LR.fitGrad(X_train, Y_train, LAMBDA, 0.001, maxEpochs = 5000)
+    Y_pred = LR.predict(W, X_test)
+    print("Best LAMBDA: ", LAMBDA)
+    print("RSS: ", LR.computeRSS(Y_pred, Y_test))
---
+++ Session 2/scikitlearn.py
@@ -0,0 +1,83 @@
+from sklearn.cluster import KMeans
+from sklearn.svm import LinearSVC, SVC
+from scipy.sparse import csr_matrix
+import numpy as np
+
+def load_data(data_path):
+    def sparse_to_dense(sparse_r_d, vocab_size):
+        r_d = [0.0 for _ in range(vocab_size)]
+        indices_tfidfs = sparse_r_d.split()
+        for index_tfidf in indices_tfidfs:
+            index = int(index_tfidf.split(':')[0])
+            tfidf = float(index_tfidf.split(':')[1])
+            r_d[index] = tfidf
+        return np.array(r_d)
+
+    with open(data_path) as f:
+        d_lines = f.read().splitlines()
+    with open('Session 1\TF-IDF Vectorizer\\20news-bydate\words_idfs.txt') as f:
+        vocab_size = len(f.read().splitlines())
+
+    data = []
+    labels = []
+    for data_id, d in enumerate(d_lines):
+        features = d.split('<fff>')
+        label, doc_id = int(features[0]), int(features[1])
+        r_d = sparse_to_dense(sparse_r_d = features[2], vocab_size=vocab_size)
+        data.append(r_d)
+        labels.append(label)
+    return data, labels
+
+def clustering_with_KMeans():
+    data, labels = load_data(data_path='Session 1\TF-IDF Vectorizer\\20news-bydate\\tf-idf.txt')
+    X = csr_matrix(data)
+    print('='*20)
+    kmeans = KMeans(
+        n_clusters=20,
+        init='random',
+        n_init=5,
+        tol=1e-3,
+        random_state=2022
+    ).fit(X)
+    labels = kmeans.labels_
+    print(labels)
+
+def compute_accuracy(predicted_y, expected_y):
+    matches = np.equal(predicted_y, expected_y)
+    accuracy = np.sum(matches.astype(float))/expected_y.size
+    return accuracy
+
+def classifying_with_linear_SVMs():
+    train_X, train_y = load_data('Session 1\TF-IDF Vectorizer\\20news-bydate\\train_tf-idf.txt')
+    train_X, train_y = np.array(train_X), np.array(train_y)
+    classifier = LinearSVC(
+        C=10.0, # penalty coefficient
+        tol=0.001, # tolerance for stopping criteria
+        verbose=False # whether prints out logs or not
+    )
+    classifier.fit(train_X, train_y)
+    test_X, test_y = load_data('Session 1\TF-IDF Vectorizer\\20news-bydate\\test_tf-idf.txt')
+    test_X, test_y = np.array(test_X), np.array(test_y)
+    predicted_y = classifier.predict(test_X)
+    accuracy = compute_accuracy(predicted_y = predicted_y, expected_y = test_y)
+    print(f'Accuracy: {accuracy}')
+
+def classifying_with_kernel_SVMs():
+    train_X, train_y = load_data('Session 1\TF-IDF Vectorizer\\20news-bydate\\train_tf-idf.txt')
+    train_X, train_y = np.array(train_X), np.array(train_y)
+    classifier = SVC(
+        C = 50.0, # penalty coefficient
+        kernel = 'rbf',
+        gamma = 0.1,
+        tol = 0.01, # tolerance for stopping criteria
+        verbose = True # whether prints out logs or not
+    )
+    classifier.fit(train_X, train_y)
+    test_X, test_y = load_data('Session 1\TF-IDF Vectorizer\\20news-bydate\\test_tf-idf.txt')
+    test_X, test_y = np.array(test_X), np.array(test_y)
+    predicted_y = classifier.predict(test_X)
+    accuracy = compute_accuracy(predicted_y = predicted_y, expected_y = test_y)
+    print(f'Accuracy: {accuracy}')
+
+if __name__=='__main__':
+    classifying_with_kernel_SVMs()
